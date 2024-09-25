a6b6fa37f16179d27f8ccf0e1011b53f475fda1b
AnaMoya <anmoco01@alumnatflorida.es>
2024-09-25 21:23:30 +0200

    Añadimos funcionalidad de suma y resta a calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,39 @@
+<?php
+function calcular($num1, $num2, $operacion)
+{
+    $resultado = 0;
+
+    switch ($operacion) {
+        case 'sumar':
+            $resultado = $num1 + $num2;
+            break;
+        case 'restar':
+            $resultado = $num1 - $num2;
+            break;
+        default:
+            $resultado = "No se puede realizar esta operación";
+            break;
+    }
+
+    return $resultado;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operacion = $_POST['operacion'];
+
+    // Llamamos a la función calcular
+    $resultado = calcular($num1, $num2, $operacion);
+
+    // Redirigimos de nuevo a la página de la calculadora con el resultado que hemos obtenido
+    header("Location: index.php?resultado=" . urlencode($resultado));
+    exit();
+
+    /*función header() de PHP para enviar una cabecera HTTP al navegador, que en este caso es una redirección (a través del código Location)
+    Redirige al usuario a la página index.php con el parámetro resultado en la URL, es decir, index.php?resultado=valor
+    urlencode($resultado) se asegura de que el valor de $resultado sea codificado de forma segura para la URL (por ejemplo, si hay espacios u otros caracteres especiales)
+    Después de redirigir, el script PHP se detiene inmediatamente gracias a la función exit();, asegurándose de que no se ejecute ningún código adicional después de la redirección
+    */
+}
+?>
